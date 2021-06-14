ba9fc0c6caca72977d593cdfa987551e068bc055
Thomas Soria <thomas.soria@gmail.com>
2021-06-14 16:49:33 -0700

    Add framework for queue and stack classes

---
+++ js/data_structures/queue.js
@@ -0,0 +1,28 @@
+import Node from "./node.js"
+
+/**
+ * Implementation of Queue using a LinkedList
+ */
+export default class Queue {
+   constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+   }
+
+   getLength() {
+   }
+
+   isEmpty() {
+   }
+
+   enqueue(element) {
+   }
+
+   dequeue(element) {
+   }
+
+   // Returns the top element of the queue without removing it
+   peek() {
+   }
+}
---
+++ js/data_structures/stack.js
@@ -0,0 +1,27 @@
+import Node from "./node.js"
+
+/**
+ * Implementation of Stack using a LinkedList
+ */
+export default class Stack {
+   constructor() {
+      this.head = null;
+      this.length = 0;
+   }
+
+   getLength() {
+   }
+
+   isEmpty() {
+   }
+
+   push(element) {
+   }
+
+   pop(element) {
+   }
+
+   // Returns the top element of the stack without removing it
+   peek() {
+   }
+}
