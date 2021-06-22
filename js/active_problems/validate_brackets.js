94a659e518f95cb3e3a35112e00f197a71e45e43
Alexandre Soria <alexx.soria@gmail.com>
2021-06-22 12:10:13 -0700

    Add 3 new ways to validate brackets

--- js/active_problems/validate_brackets.js
+++ js/active_problems/validate_brackets.js
@@ -10,19 +10,19 @@
  * If close, then remove from array
  * {[(])}
  *
- *
+ *Feed brackets into helpers
+ * Make functions variables
  * */
 
 function parity(string) {
-  let i = 0;
-  for (i; i < string.length; i++) {
+  for (let i = 0; i < string.length; i++) {
     if (string[i] == "}" || string[i] == "]" || string[i] == ")") {
       return false;
     }
 
     if (string[i] == "(") {
       if (isClosedParens(i) == false) {
-          return false;
+        return false;
       }
       i--;
     }
@@ -30,7 +30,7 @@ function parity(string) {
     if (string[i] == "[") {
       if (isClosedBrackets(i) == false) {
         return false;
-        }
+      }
       i--;
     }
     if (string[i] == "{") {
@@ -45,27 +45,27 @@ function parity(string) {
     for (let advance = i + 1; advance < string.length; advance++) {
       if (string[advance] == "}" || string[advance] == "]") {
         return false;
-      } 
+      }
 
       if (string[advance] == ")") {
-        string  = string.slice(0, i) + string.slice(advance + 1);
+        string = string.slice(0, i) + string.slice(advance + 1);
         return;
       }
 
       if (string[advance] == "(") {
         if (isClosedParens(advance) == false) {
-            return false;
+          return false;
         }
       }
 
       if (string[advance] == "[") {
         if (isClosedBrackets(advance) == false) {
-            return false;
+          return false;
         }
       }
       if (string[advance] == "{") {
         if (isClosedCurly(advance) == false) {
-            return false;
+          return false;
         }
       }
     }
@@ -75,62 +75,121 @@ function parity(string) {
     for (let advance = i + 1; advance < string.length; advance++) {
       if (string[advance] == "}" || string[advance] == ")") {
         return false;
-      } 
+      }
       if (string[advance] == "]") {
-        string  = string.slice(0, i) + string.slice(advance + 1);
-        return;      
+        string = string.slice(0, i) + string.slice(advance + 1);
+        return;
       }
 
       if (string[advance] == "(") {
         if (isClosedParens(advance) == false) {
-            return false;
+          return false;
         }
       }
 
       if (string[advance] == "[") {
         if (isClosedBrackets(advance) == false) {
-            return false;
+          return false;
         }
       }
       if (string[advance] == "{") {
         if (isClosedCurly(advance) == false) {
-            return false;
+          return false;
         }
       }
     }
   }
-
 
   function isClosedCurly(i) {
     for (let advance = i + 1; advance < string.length; advance++) {
       if (string[advance] == ")" || string[advance] == "]") {
         return false;
-      } 
-      
+      }
+
       if (string[advance] == "}") {
-        string  = string.slice(0, i) + string.slice(advance + 1);
-        return;      
+        string = string.slice(0, i) + string.slice(advance + 1);
+        return;
       }
 
       if (string[advance] == "(") {
         if (isClosedParens(advance) == false) {
-            return false;
+          return false;
         }
       }
 
       if (string[advance] == "[") {
         if (isClosedBrackets(advance) == false) {
-            return false;
+          return false;
         }
       }
       if (string[advance] == "{") {
         if (isClosedCurly(advance) == false) {
-            return false;
+          return false;
         }
       }
     }
   }
-
   return true;
 }
-console.log(parity('a(b[c{d}e]f)g'))
+function validateBrackets(string) {
+  let stack = [];
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == "(" || string[i] == "{" || string[i] == "[") {
+      stack.push(string[i]);
+    } else if (string[i] == ")" && stack.pop() != "(") {
+      return false;
+    } else if (string[i] == "}" && stack.pop() != "{") {
+      return false;
+    } else if (string[i] == "]" && stack.pop() != "[") {
+      return false;
+    }
+  }
+  return stack.length == 0;
+}
+
+
+function validateBrackets2(string) {
+  let stack = [];
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+      case '[':
+        stack.push(']');
+        break;
+
+      case '{':
+        stack.push('}');
+        break;
+
+      case '(':
+        stack.push(')');
+        break;
+
+      default:
+        if (stack.pop() != string[i]) {
+          return false;
+        }
+    }
+  }
+  return stack.length == 0;
+}
+
+var isValid = function(s) {   
+  const stack = [];
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+  
+  for (let i = 0 ; i < s.length ; i++) {
+      const c = s[i];
+      if (map[c]) {
+        stack.push(map[c])
+      } else if (c !== stack.pop()) {
+        return false;
+      } 
+  }
+  
+  return !stack.length;
+};
+console.log(validateBrackets("({[("));
