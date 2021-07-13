d0682c6620d8312279fa4c554271649b96138303
Alexandre Soria <alexx.soria@gmail.com>
2021-07-13 12:24:28 -0700

    Add better solution to rain collector

--- js/active_problems/Rain_collector.js
+++ js/active_problems/Rain_collector.js
@@ -2,6 +2,26 @@
  * map where the width of each bar is 1, 
  * compute how much water it can trap after raining. */
 
+function rainCatcher(array){
+    let collection = 0;
+
+    let leftMax = array[0];
+    let rightMax = array[array.length - 1];
+    let left = 1;
+    let right = array.length - 1;
+
+    while (left < right) {
+        if (array[left] < array[right]) {
+            array[left] >= leftMax ? leftMax = array[left] : collection += (leftMax - array[left]);
+            left++;
+        } else {
+            array[right] >= rightMax ? rightMax = array[right] : collection += (rightMax - array[right]);
+            right--;
+        }
+    }
+    return collection;
+}
+
 function collector(array) {
     let length = array.length;
     if (length < 3) {
@@ -12,7 +32,6 @@ function collector(array) {
     let leftMax = array[0];
 
     for (let i = 1; i < length - 1; i ++) {
-        console.log("leftMax = " + leftMax);
         if (array[i] < leftMax){
             let rightMax = 0;
             for (let j = i + 1; j < length; j++) {
@@ -20,8 +39,6 @@ function collector(array) {
                     rightMax = array[j];
                 }
             }
-            console.log("rightMax = " + rightMax);
-            console.log("array[i] = " + array[i]);
             if (array[i] < rightMax) {
                 if (leftMax > rightMax) {
                     collection += rightMax - array[i];
@@ -38,6 +55,6 @@ function collector(array) {
     return collection;
 }
 
-const array = [0, 5, 3, 2, 6, 7, 2, 3, 0, 4, 0];
+const array = [0, 5, 3, 2, 6, 7, 2, 3, 1, 4, 0];
 
-console.log(collector(array));
+console.log(rainCatcher(array));
