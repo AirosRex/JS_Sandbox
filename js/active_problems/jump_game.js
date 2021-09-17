c3193133806cfe7f6d4899008140e73888372c4c
Alexandre Soria <alexx.soria@gmail.com>
2021-09-16 21:42:40 -0700

    Add first solution to jump_game.

--- js/active_problems/jump_game.js
+++ js/active_problems/jump_game.js
@@ -4,13 +4,38 @@ Each element in the array represents your maximum jump length at that position.
 
 Your goal is to reach the last index in the minimum number of jumps.
 
-You can assume that you can always reach the last index.*/
+You can assume that you can always reach the last index.
+
+Thoughts:
+Give value of "Distance from end" to values in the back and work backwards. 
+Try values previous to the end and see who can make it in one jump.
+Whoever the first one is becomes new end. 
+Repeat until i = 0;
+
+
+*/
 
 function jump (nums) {
-let lowestFinish = nums.length;
-
-return lowestFinish;
+    if (nums.length < 2) {
+        return "Please input array with two or more integers";
+    }
+    let counter = 0;
+    let finished = false;
+    let goal = nums.length - 1;
+    while (finished == false) {
+        console.log(goal);
+        for (let i = 0; i < goal; i++){
+            if (i + nums[i] >= goal){
+                if (i == 0) {
+                    finished = true;
+                }
+                goal = i;
+                counter++;
+            }
+        }
+    }
+    return counter;
 }
 
-const nums = [2,3,1,1,2,2,1]
-console.log(jump);
+const nums = [2,2,1,5,8,3,2,5,6,5,4,2,1,4,6,7,8,9,,1,2,2,1];
+console.log(jump(nums));
